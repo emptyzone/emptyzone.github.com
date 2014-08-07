3ecc77e8619d8d0506a54aff89f098a331a4116c
songchenwen <emptyzone.0@gmail.com>
2014-08-08 00:03:10 +0800

    use express server

--- app.js
+++ app.js
@@ -4,42 +4,40 @@ var repository_name = 'emptyzone.github.com';
 
 var sys = require('sys'),
     exec = require('child_process').exec;
-var app = require('http').createServer(handler);
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
 var port = Number(process.env.PORT || 4000);
 
-sys.puts("listening to : " + port);
-app.listen(port);
+app.use(bodyParser.json());
+app.get('/', function(req, res){
+            sys.puts('POST DATA NOT ACCEPTED');
+            res.send('success');
+        });
 
-function handler (req, res) {
-    if (req.method == "POST") {
-        var data = '';
-        req.on('data', function(chunk) {
-               data += chunk;
-            });
-        req.on('end', function() {
-               sys.puts('Received body data:');
-               data = JSON.parse(data);
-               if(data.action == 'opened' &&
-                   data.issue &&
-                   data.issue.title &&
-                   data.issue.title.indexOf(issue_key_word) != -1 &&
-                   data.issue.user &&
-                   data.issue.user.login &&
-                   data.issue.user.login == user_name &&
-                   data.repository &&
-                   data.repository.name &&
-                   data.repository.name == repository_name){
-                    exec('hexo migrate gist; hexo d; hexo clean;', puts);
-               }else{
-                    sys.puts('POST DATA NOT ACCEPTED');
-               }
-            });
-    }else{
-        sys.puts('ABORT GET REQUEST');
-    }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("success");
-    res.end();
-}
+app.post('/', function(req, res){
+            var data = req.body;
+            if(data.action == 'opened' &&
+               data.issue &&
+               data.issue.title &&
+               data.issue.title.indexOf(issue_key_word) != -1 &&
+               data.issue.user &&
+               data.issue.user.login &&
+               data.issue.user.login == user_name &&
+               data.repository &&
+               data.repository.name &&
+               data.repository.name == repository_name){
+                var respond = '';
+                exec('hexo migrate gist; hexo d; hexo clean;', function(error, stdout, stderr){
+                        sys.puts(stdout);
+                        respond += stdout;
+                        res.send(respond);
+                     });
+            }else{
+                res.send('not valid data');
+            }
+         });
 
-function puts(error, stdout, stderr) {sys.puts(stdout)}
+app.listen(port, function(){
+            sys.puts("listening to : " + port);
+           });
